0e9f00b5aab7f0f0f0e0737b557e9e5ffa6b041f
Jaggwagg <jaggwagg@gmail.com>
2023-01-13 09:56:42 -0600

    Removed unnecessary client files

--- fabric/src/main/java/jaggwagg/frozen_apocalypse/mixin/client/ClientApocalypseEffects.java
+++
@@ -1,8 +0,0 @@
-package jaggwagg.frozen_apocalypse.mixin.client;
-
-import net.minecraft.client.render.BackgroundRenderer;
-import net.minecraft.client.render.WorldRenderer;
-
-public class ClientApocalypseEffects {
-    //WorldRenderer
-}
