a6166dd9016d3e8294fd1055e802a911a9b0d6e2
Jaggwagg <jaggwagg@gmail.com>
2023-11-29 20:50:09 -0600

    Made category text aqua

--- src/main/java/jaggwagg/frozen_apocalypse/world/FrozenApocalypseGameRules.java
+++ src/main/java/jaggwagg/frozen_apocalypse/world/FrozenApocalypseGameRules.java
@@ -11,7 +11,7 @@ import net.minecraft.world.GameRules;
 
 public class FrozenApocalypseGameRules {
     public static final Identifier CATEGORY_IDENTIFIER = new Identifier(FrozenApocalypse.MOD_ID, FrozenApocalypse.MOD_ID);
-    public static final CustomGameRuleCategory CATEGORY = new CustomGameRuleCategory(CATEGORY_IDENTIFIER, Text.translatable("gamerule.category." + FrozenApocalypse.MOD_ID).formatted(Formatting.YELLOW, Formatting.BOLD));
+    public static final CustomGameRuleCategory CATEGORY = new CustomGameRuleCategory(CATEGORY_IDENTIFIER, Text.translatable("gamerule.category." + FrozenApocalypse.MOD_ID).formatted(Formatting.AQUA, Formatting.BOLD));
     public static final GameRules.Key<GameRules.IntRule> FROZEN_APOCALYPSE_LEVEL =
             GameRuleRegistry.register("frozenApocalypseLevel", CATEGORY, GameRuleFactory.createIntRule(0));
     public static final GameRules.Key<GameRules.BooleanRule> FROZEN_APOCALYPSE_LEVEL_OVERRIDE =
