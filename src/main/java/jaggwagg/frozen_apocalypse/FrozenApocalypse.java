6cea05635b096247902c9267355e5ce05f6c5c5f
Jaggwagg <jaggwagg@gmail.com>
2023-12-21 16:30:11 -0600

    Fixed inconsistency accessing the config

--- src/main/java/jaggwagg/frozen_apocalypse/FrozenApocalypse.java
+++ src/main/java/jaggwagg/frozen_apocalypse/FrozenApocalypse.java
@@ -16,7 +16,6 @@ public class FrozenApocalypse implements ModInitializer {
     public static final String MOD_ID = "frozen_apocalypse";
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
     public static final FrozenApocalypseConfig CONFIG = FrozenApocalypseConfig.getConfig();
-    public static final boolean FROZEN_APOCALYPSE_ENABLED = CONFIG.getFrozenApocalypseEnabled();
     public static int frozenApocalypseLevel = 0;
 
     @Override
