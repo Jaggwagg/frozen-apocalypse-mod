cb845328987d703e29a0640e0056fdbcee7e57b5
Jaggwagg <jaggwagg@gmail.com>
2024-01-10 18:34:19 -0600

    Fixed only one type of thermal armor protecting the player

--- src/main/java/jaggwagg/frozen_apocalypse/item/ThermalArmorItem.java
+++ src/main/java/jaggwagg/frozen_apocalypse/item/ThermalArmorItem.java
@@ -1,11 +1,9 @@
 package jaggwagg.frozen_apocalypse.item;
 
-import jaggwagg.frozen_apocalypse.registry.ModItems;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.Item;
 
 public class ThermalArmorItem extends ArmorItem {
     public ThermalArmorItem(ArmorMaterial material, Type type, Settings settings) {
@@ -13,18 +11,13 @@ public class ThermalArmorItem extends ArmorItem {
     }
 
     public static boolean wearingThermalArmor(LivingEntity livingEntity) {
-        Item thermalBoots = ModItems.RegisteredItems.IRON_THERMAL_BOOTS.getItem();
-        Item thermalLeggings = ModItems.RegisteredItems.IRON_THERMAL_LEGGINGS.getItem();
-        Item thermalChestplate = ModItems.RegisteredItems.IRON_THERMAL_CHESTPLATE.getItem();
-        Item thermalHelmet = ModItems.RegisteredItems.IRON_THERMAL_HELMET.getItem();
-
-        return isWearingItem(livingEntity, EquipmentSlot.FEET, thermalBoots) &&
-                isWearingItem(livingEntity, EquipmentSlot.LEGS, thermalLeggings) &&
-                isWearingItem(livingEntity, EquipmentSlot.CHEST, thermalChestplate) &&
-                isWearingItem(livingEntity, EquipmentSlot.HEAD, thermalHelmet);
+        return isWearingThermalArmorItem(livingEntity, EquipmentSlot.FEET) &&
+                isWearingThermalArmorItem(livingEntity, EquipmentSlot.LEGS) &&
+                isWearingThermalArmorItem(livingEntity, EquipmentSlot.CHEST) &&
+                isWearingThermalArmorItem(livingEntity, EquipmentSlot.HEAD);
     }
 
-    private static boolean isWearingItem(LivingEntity livingEntity, EquipmentSlot slot, Item item) {
-        return livingEntity.getEquippedStack(slot).getItem() == item;
+    private static boolean isWearingThermalArmorItem(LivingEntity livingEntity, EquipmentSlot slot) {
+        return livingEntity.getEquippedStack(slot).getItem() instanceof ThermalArmorItem;
     }
 }
