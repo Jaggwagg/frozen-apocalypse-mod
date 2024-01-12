97b2ee33ec6edc77106e1de8a42334f192337e9b
Jaggwagg <jaggwagg@gmail.com>
2024-01-12 11:29:17 -0600

    Added more special mob effects

--- src/main/java/jaggwagg/frozen_apocalypse/entity/mob/CryoboomerEntity.java
+++ src/main/java/jaggwagg/frozen_apocalypse/entity/mob/CryoboomerEntity.java
@@ -1,11 +1,136 @@
 package jaggwagg.frozen_apocalypse.entity.mob;
 
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.AreaEffectCloudEntity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
+
+import java.util.Collection;
 
 public class CryoboomerEntity extends CreeperEntity {
+    private static final TrackedData<Integer> FUSE_SPEED = DataTracker.registerData(CryoboomerEntity.class, TrackedDataHandlerRegistry.INTEGER);
+    private static final TrackedData<Boolean> CHARGED = DataTracker.registerData(CryoboomerEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
+    private static final TrackedData<Boolean> IGNITED = DataTracker.registerData(CryoboomerEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
+    private int currentFuseTime;
+    @SuppressWarnings("all")
+    private int lastFuseTime;
+
     public CryoboomerEntity(EntityType<? extends CryoboomerEntity> entityType, World world) {
         super(entityType, world);
+    }
+
+    private void spawnEffectsCloud() {
+        Collection<StatusEffectInstance> collection = this.getStatusEffects();
+        AreaEffectCloudEntity slownessAreaEffectCloudEntity = createAreaEffectCloudEntity(7.0f);
+
+        slownessAreaEffectCloudEntity.addEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 100, 1));
+        this.getWorld().spawnEntity(slownessAreaEffectCloudEntity);
+
+        if (!collection.isEmpty()) {
+            AreaEffectCloudEntity areaEffectCloudEntity = createAreaEffectCloudEntity(2.5f);
+
+            for (StatusEffectInstance statusEffectInstance : collection) {
+                areaEffectCloudEntity.addEffect(new StatusEffectInstance(statusEffectInstance));
+            }
+
+            this.getWorld().spawnEntity(areaEffectCloudEntity);
+        }
+    }
+
+    private AreaEffectCloudEntity createAreaEffectCloudEntity(float radius) {
+        AreaEffectCloudEntity areaEffectCloudEntity = new AreaEffectCloudEntity(this.getWorld(), this.getX(), this.getY(), this.getZ());
+        areaEffectCloudEntity.setRadius(radius);
+        areaEffectCloudEntity.setRadiusOnUse(-0.5f);
+        areaEffectCloudEntity.setWaitTime(10);
+        areaEffectCloudEntity.setDuration(areaEffectCloudEntity.getDuration() / 2);
+        areaEffectCloudEntity.setRadiusGrowth(-areaEffectCloudEntity.getRadius() / (float) areaEffectCloudEntity.getDuration());
+        return areaEffectCloudEntity;
+    }
+
+    private void placePowderedSnow(BlockPos blockPos) {
+        int radius = 10;
+        int center = (int) (radius * 0.5);
+        int squareDistance; 
+
+        if (!this.getWorld().getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING)) {
+            return;
+        }
+
+        for (int x = center - radius; x < radius - center; x++) {
+            for (int y = center - radius; y < radius - center; y++) {
+                for (int z = center - radius; z < radius - center; z++) {
+                    squareDistance = x * x + y * y + z * z;
+
+                    if (squareDistance < (radius - center) * (radius - center)) {
+                        BlockPos placingBlockPos = new BlockPos(blockPos.getX() + x, blockPos.getY() + y, blockPos.getZ() + z);
+
+                        if (!this.getWorld().getBlockState(placingBlockPos).isAir()) {
+                            if (this.getWorld().getBlockState(placingBlockPos.up()).isAir()) {
+                                this.getWorld().setBlockState(placingBlockPos, Blocks.POWDER_SNOW.getDefaultState());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void explode() {
+        if (!this.getWorld().isClient) {
+            float charged = this.shouldRenderOverlay() ? 2.0F : 1.0F;
+            this.dead = true;
+            int explosionRadius = 3;
+            this.getWorld().createExplosion(this, this.getX(), this.getY(), this.getZ(), (float) explosionRadius * charged, World.ExplosionSourceType.MOB);
+            this.discard();
+            this.spawnEffectsCloud();
+            this.placePowderedSnow(this.getBlockPos());
+        }
+    }
+
+    @Override
+    protected void initDataTracker() {
+        super.initDataTracker();
+        this.dataTracker.startTracking(FUSE_SPEED, -1);
+        this.dataTracker.startTracking(CHARGED, false);
+        this.dataTracker.startTracking(IGNITED, false);
+    }
+
+    @Override
+    public void tick() {
+        if (this.isAlive()) {
+            this.lastFuseTime = this.currentFuseTime;
+            if (this.isIgnited()) {
+                this.setFuseSpeed(1);
+            }
+
+            int i = this.getFuseSpeed();
+            if (i > 0 && this.currentFuseTime == 0) {
+                this.playSound(SoundEvents.ENTITY_CREEPER_PRIMED, 1.0F, 0.5F);
+                this.emitGameEvent(GameEvent.PRIME_FUSE);
+            }
+
+            this.currentFuseTime += i;
+            if (this.currentFuseTime < 0) {
+                this.currentFuseTime = 0;
+            }
+
+            int fuseTime = 30;
+            if (this.currentFuseTime >= fuseTime) {
+                this.currentFuseTime = fuseTime;
+                this.explode();
+            }
+        }
+
+        super.tick();
     }
 }
---
+++ src/main/java/jaggwagg/frozen_apocalypse/mixin/entity/mob/EndermanEntityMixin.java
@@ -0,0 +1,39 @@
+package jaggwagg.frozen_apocalypse.mixin.entity.mob;
+
+import jaggwagg.frozen_apocalypse.entity.mob.ShiverstareEntity;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.mob.EndermanEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EndermanEntity.class)
+public class EndermanEntityMixin {
+    @Inject(method = "isPlayerStaring", at = @At("HEAD"))
+    private void isPlayerStaring(PlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
+        EndermanEntity endermanEntity = (EndermanEntity) (Object) this;
+        ItemStack itemStack = player.getInventory().armor.get(3);
+
+        if (!itemStack.isOf(Blocks.CARVED_PUMPKIN.asItem())) {
+            Vec3d vec3d = player.getRotationVec(1.0F).normalize();
+            Vec3d vec3d2 = new Vec3d(endermanEntity.getX() - player.getX(), endermanEntity.getEyeY() - player.getEyeY(), endermanEntity.getZ() - player.getZ());
+            double d = vec3d2.length();
+            vec3d2 = vec3d2.normalize();
+            double e = vec3d.dotProduct(vec3d2);
+
+            if (e > 1.0 - 0.025 / d) {
+                if (player.canSee(endermanEntity)) {
+                    if (endermanEntity instanceof ShiverstareEntity) {
+                        player.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 100, 2));
+                    }
+                }
+            }
+        }
+    }
+}
