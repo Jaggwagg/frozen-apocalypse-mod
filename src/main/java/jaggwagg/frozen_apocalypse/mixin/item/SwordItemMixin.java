a16a683fd63772ff4775237609b988bac8718c91
Jaggwagg <jaggwagg@gmail.com>
2024-01-15 10:43:34 -0600

    Fixed icy cobwebs not breaking fast with swords or shears

---
+++ src/main/java/jaggwagg/frozen_apocalypse/mixin/item/SwordItemMixin.java
@@ -0,0 +1,20 @@
+package jaggwagg.frozen_apocalypse.mixin.item;
+
+import jaggwagg.frozen_apocalypse.registry.ModBlocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SwordItem.class)
+public class SwordItemMixin {
+    @Inject(method = "getMiningSpeedMultiplier", at = @At("RETURN"), cancellable = true)
+    public void getMiningSpeedMultiplier(ItemStack stack, BlockState state, CallbackInfoReturnable<Float> cir) {
+        if (state.isOf(ModBlocks.RegisteredBlocks.ICY_COBWEB.getBlock())) {
+            cir.setReturnValue(15.0f);
+        }
+    }
+}
