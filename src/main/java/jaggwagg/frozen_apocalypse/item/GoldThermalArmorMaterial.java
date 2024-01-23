3e3939201cd532bb355aa6183c094676f32f4bf1
Jaggwagg <jaggwagg@gmail.com>
2024-01-23 15:47:26 -0600

    Added golden thermal armor

---
+++ src/main/java/jaggwagg/frozen_apocalypse/item/GoldThermalArmorMaterial.java
@@ -0,0 +1,53 @@
+package jaggwagg.frozen_apocalypse.item;
+
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+
+public class GoldThermalArmorMaterial implements ArmorMaterial {
+    private static final int[] BASE_DURABILITY = new int[]{13, 15, 16, 11};
+    private static final int[] PROTECTION_VALUES = new int[]{2, 5, 3, 1};
+
+    @Override
+    public int getDurability(ArmorItem.Type type) {
+        return BASE_DURABILITY[type.getEquipmentSlot().getEntitySlotId()] * 7;
+    }
+
+    @Override
+    public int getProtection(ArmorItem.Type type) {
+        return PROTECTION_VALUES[type.getEquipmentSlot().getEntitySlotId()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 25;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return SoundEvents.ITEM_ARMOR_EQUIP_GOLD;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(Items.GOLD_INGOT);
+    }
+
+    @Override
+    public String getName() {
+        return "golden_thermal";
+    }
+
+    @Override
+    public float getToughness() {
+        return 0.0f;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return 0.0f;
+    }
+}
